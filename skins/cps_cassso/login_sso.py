a9123d2e3954e34c3054a89c764182ac05097c90
jsautret <devnull@localhost>
2004-08-27 13:52:39 +0000

    logged_in is enough, logged_in_sso can be removed.

--- skins/cps_cassso/login_sso.py
+++ skins/cps_cassso/login_sso.py
@@ -1,8 +1,16 @@
+## Script (Python) "login_sso"
+##title=Login SSO
 ##parameters=came_from=None
+
+# $Id$
+
+""" Replace the login_form and redirect to the CAS login form.
+login_sso should remplace login_form as the "Auto-login page ID"
+Cookie Crumbler parameter. """
 
 import urllib
 
-logged_in=context.portal_url() + '/logged_in_sso'
+logged_in=context.portal_url() + '/logged_in'
 if came_from:
     logged_in += "?came_from=%s" % came_from
 
